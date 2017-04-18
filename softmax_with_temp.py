1cd559b11f2c0b76454fba774e329d65a4c0d921
Tejas Godambe <tejas.godambe@gmail.com>
2017-04-18 13:50:42 +0530

    test

--- softmax_with_temp.py
+++ softmax_with_temp.py
@@ -1,6 +1,7 @@
 import theano
 import numpy as np
 
+## Softmax function with temperature parameter
 def softmax_with_temp(x):
     Temp = 2.0
     e_x = np.exp((x - x.max(axis=1, keepdims=True))/Temp)
